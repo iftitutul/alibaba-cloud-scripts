9d3404fa73094089cf9ad341f244ba8f61ccbfec
Iftikhar Hossain <iftikhar.tutul@gmail.com>
2026-01-08 21:37:00 +0300

    Add profile argument to ECS inventory script

--- ecs-inventory/run.sh
+++ ecs-inventory/run.sh
@@ -2,5 +2,5 @@ python3 -m venv venv
 source venv/bin/activate
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
-python3 alibaba-ecs-inventory.py
+python3 alibaba-ecs-inventory.py --profile $1
 
